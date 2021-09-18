9fb2715208291ce8b24f83137add17107b23fcb9
Dario Navarrete <rd_navarrete@outlook.com>
2021-09-17 23:18:57 -0500

    Finalizacion modulo Error y Preguntas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import React, { useState } from "react";
+import Pregunta from "./components/Pregunta";
+
 function App() {
-  return <h1>Presupuesto</h1>;
+  //Definir los state
+
+  const [presupuesto, guardarPresupuesto] = useState(0);
+  const [restante, guardarRestante] = useState(0);
+
+  return (
+    <div className="container">
+      <header>
+        <h1>Gasto Semanal</h1>
+
+        <div className="contenido-principal contenido">
+          <Pregunta
+            guardarPresupuesto={guardarPresupuesto}
+            guardarRestante={guardarRestante}
+          />
+
+          <div className="row">
+            <div className="one-half column">1</div>
+            <div className="one-half column">2</div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
